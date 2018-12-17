c7171410d31ea7c1530c922601f163843ad5ec22
Matthew Goff <mtthwgff@gmail.com>
2018-12-17 17:43:35 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    private static readonly float MIN_CAMERA_HEIGHT = 1f;
+    private static readonly float MAX_CAMERA_HEIGHT = 60f;
+    private static readonly float MAX_CAMERA_WIDTH = 120f;
+    private static readonly float ZOOM_SPEED = 1f;//1.2f;
+    private static readonly float PAN_SPEED = 0f;//1f;
+
+    public static CameraController instance;
+
+    public RectTransform UI;
+    private Camera Camera;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        Camera = GetComponent<Camera>();
+        float ppu = Camera.pixelWidth / (2 * Camera.orthographicSize * Camera.aspect);
+        float uiWidth = UI.rect.width;
+        float cameraOffset = -uiWidth / (2 * ppu);
+        transform.position += new Vector3(cameraOffset, 0, 0);
+    }
+
+    private void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Camera.transform.position += PAN_SPEED * movement;
+
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            Camera.orthographicSize *= (1f / ZOOM_SPEED);
+            if (Camera.orthographicSize < 1f)
+            {
+                Camera.orthographicSize = 1f;
+            }
+        }
+        else if (d < 0f)
+        {
+            Camera.orthographicSize *= ZOOM_SPEED;
+            if (Camera.orthographicSize > 30f)
+            {
+                Camera.orthographicSize = 30f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public static Vector2 GetMousePosition()
+    {
+        if (instance == null)
+        {
+            return Vector2.zero;
+        }
+        else
+        {
+            return instance.Camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnPostRender()
+    {
+        DrawGridLines();
+        if (GameManager.instance.ValidInputs)
+        {
+            DrawVectorLines();
+        }
+    }
+
+    private void DrawVectorLines()
+    {
+        Vector2 targetPosition = GameManager.instance.TargetPosition;
+        Vector2 velocity = GameManager.instance.Velocity;
+        Vector2 tangentVelocity = targetPosition * Vector2.Dot(targetPosition, velocity) / Mathf.Pow(targetPosition.magnitude, 2);
+        Vector2 parallelVelocity = velocity - tangentVelocity;
+        Vector2 heading = GameManager.instance.Heading;
+        Vector2 desiredHeading;
+        if (GameManager.instance.DesiredHeading != GameManager.instance.DesiredHeading)
+        {
+            desiredHeading = Vector2.zero;
+        }
+        else
+        {
+            float targetAngle = Vector2.SignedAngle(Vector2.right, targetPosition);
+            desiredHeading = Quaternion.Euler(0f, 0f, GameManager.instance.DesiredHeading + targetAngle) * Vector2.right;
+        }
+
+        Material lineMat = new Material(Shader.Find("Sprites/Default"));
+        GL.Begin(GL.LINES);
+        lineMat.SetPass(0);
+
+        // Target Position
+        GL.Color(new Color(1f, 0f, 0f, 1f));
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(targetPosition.x, targetPosition.y, 0f);
+
+        // Velocity
+        GL.Color(new Color(0f, 1f, 0f, 1f));
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(velocity.x, velocity.y, 0f);
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(tangentVelocity.x, tangentVelocity.y, 0f);
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(parallelVelocity.x, parallelVelocity.y, 0f);
+
+        // Heading
+        GL.Color(new Color(0f, 0f, 1f, 1f));
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(heading.x, heading.y, 0f);
+
+        // Desired Heading
+        GL.Color(new Color(0f, 1f, 1f, 1f));
+        GL.Vertex3(0f, 0f, 0f);
+        GL.Vertex3(desiredHeading.x, desiredHeading.y, 0f);
+        GL.End();
+    }
+
+    private void DrawGridLines()
+    {
+        float height = 2f * Camera.orthographicSize;
+        float width = height * Camera.aspect;
+        float top = transform.position.y + height / 2f;
+        float bottom = transform.position.y - height / 2f;
+        float left = transform.position.x - width / 2f;
+        float right = transform.position.x + width / 2f;
+
+        Material lineMat = new Material(Shader.Find("Sprites/Default"));
+        GL.Begin(GL.LINES);
+        lineMat.SetPass(0);
+        Draw1sGrid(top, bottom, left, right);
+        Draw10sGrid(top, bottom, left, right);
+        GL.End();
+    }
+
+    private void Draw1sGrid(float top, float bottom, float left, float right)
+    {
+        GL.Color(new Color(1f, 1f, 1f, 0.2f));
+
+        for (int i = Mathf.CeilToInt(left); i < right; i++)
+        {
+            GL.Vertex3(i, bottom, 0f);
+            GL.Vertex3(i, top, 0f);
+        }
+
+        for (int i = Mathf.CeilToInt(bottom); i < top; i++)
+        {
+            GL.Vertex3(left, i, 0f);
+            GL.Vertex3(right, i, 0f);
+        }
+    }
+
+    private void Draw10sGrid(float top, float bottom, float left, float right)
+    {
+        GL.Color(new Color(1f, 1f, 1f, 1f));
+
+        for (int i = Mathf.CeilToInt(left / 10) * 10; i < right; i += 10)
+        {
+            GL.Vertex3(i, bottom, 0f);
+            GL.Vertex3(i, top, 0f);
+        }
+
+        for (int i = Mathf.CeilToInt(bottom / 10) * 10; i < top; i += 10)
+        {
+            GL.Vertex3(left, i, 0f);
+            GL.Vertex3(right, i, 0f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,176 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public RectTransform UI;
+
+    public GameObject TargetPositionXField;
+    public GameObject TargetPositionYField;
+    public GameObject VelocityXField;
+    public GameObject VelocityYField;
+    public GameObject HeadingXField;
+    public GameObject HeadingYField;
+    public GameObject AccelerationField;
+    public GameObject TurnRateField;
+
+    public GameObject ToggleGroup;
+    public GameObject TargetPositionToggle;
+    public GameObject VelocityToggle;
+    public GameObject HeadingToggle;
+
+    public GameObject WarningText;
+
+    public GameObject DesiredHeadingOutputText;
+    public GameObject ThrustOutputText;
+    public GameObject BreakOutputText;
+    public GameObject TurnOutputText;
+
+    public Vector2 TargetPosition { get; private set; }
+    public Vector2 Velocity { get; private set; }
+    public Vector2 Heading { get; private set; }
+    public float DesiredHeading { get; private set; }
+    private float Acceleration;
+    private float TurnRate;
+
+    public bool ValidInputs { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+	
+	private void Update ()
+    {
+        HandleClickEvents();
+        ValidInputs = GatherInputs();
+
+        if (ValidInputs)
+        {
+            WarningText.SetActive(false);
+            RunAutopilot();
+        }
+        else
+        {
+            WarningText.SetActive(true);
+            DesiredHeadingOutputText.GetComponent<Text>().text = "(DesiredHeading = -)";
+            ThrustOutputText.GetComponent<Text>().text = "Thrust: -";
+            BreakOutputText.GetComponent<Text>().text = "Break: -";
+            TurnOutputText.GetComponent<Text>().text = "Turn: -";
+        }
+	}
+
+    private void HandleClickEvents()
+    {
+        if (Input.GetMouseButton(0) && Input.mousePosition.x > UI.rect.width)
+        {
+            Vector2 clickPosition = CameraController.GetMousePosition();
+            Toggle toggleGroup = ToggleGroup.GetComponent<ToggleGroup>().ActiveToggles().FirstOrDefault();
+            if (toggleGroup == TargetPositionToggle.GetComponent<Toggle>())
+            {
+                TargetPositionXField.GetComponent<InputField>().text = clickPosition.x.ToString();
+                TargetPositionYField.GetComponent<InputField>().text = clickPosition.y.ToString();
+            }
+            else if (toggleGroup == VelocityToggle.GetComponent<Toggle>())
+            {
+                VelocityXField.GetComponent<InputField>().text = clickPosition.x.ToString();
+                VelocityYField.GetComponent<InputField>().text = clickPosition.y.ToString();
+            }
+            else if (toggleGroup == HeadingToggle.GetComponent<Toggle>())
+            {
+                HeadingXField.GetComponent<InputField>().text = clickPosition.x.ToString();
+                HeadingYField.GetComponent<InputField>().text = clickPosition.y.ToString();
+            }
+        }
+    }
+    
+    private bool GatherInputs()
+    {
+        float targetPositionX = 0f;
+        float targetPositionY = 0f;
+        float velocityX = 0f;
+        float velocityY = 0f;
+        float headingX = 0f;
+        float headingY = 0f;
+        if (!float.TryParse(TargetPositionXField.GetComponent<InputField>().text, out targetPositionX)
+            || !float.TryParse(TargetPositionYField.GetComponent<InputField>().text, out targetPositionY)
+            || !float.TryParse(VelocityXField.GetComponent<InputField>().text, out velocityX)
+            || !float.TryParse(VelocityYField.GetComponent<InputField>().text, out velocityY)
+            || !float.TryParse(HeadingXField.GetComponent<InputField>().text, out headingX)
+            || !float.TryParse(HeadingYField.GetComponent<InputField>().text, out headingY)
+            || !float.TryParse(AccelerationField.GetComponent<InputField>().text, out Acceleration)
+            || !float.TryParse(TurnRateField.GetComponent<InputField>().text, out TurnRate))
+        {
+            return false;
+        }
+        else
+        {
+            TargetPosition = new Vector2(targetPositionX, targetPositionY);
+            Velocity = new Vector2(velocityX, velocityY);
+            Heading = new Vector2(headingX, headingY);
+            return true;
+        }
+    }
+
+    private void RunAutopilot()
+    {
+        bool thrustOutput = false;
+        bool breakOutput = false;
+        float turnOutput = 0f;
+
+        Vector2 targetBearing = TargetPosition;
+        Vector2 parallelVelocity = targetBearing * Vector2.Dot(targetBearing, Velocity) / Mathf.Pow(targetBearing.magnitude, 2f);
+        Vector2 tangentVelocity = Velocity - parallelVelocity;
+        float headingAngle = Vector2.SignedAngle(targetBearing, Heading);
+        float tangentAngle = Vector2.SignedAngle(targetBearing, tangentVelocity);
+
+        if (Vector2.Angle(targetBearing, Velocity) > 45f)
+        {
+            breakOutput = true;
+        }
+        if (headingAngle < 45f && Vector2.Dot(Heading, Velocity) < 0f)
+        {
+            thrustOutput = true;
+        }
+        if (headingAngle < 90f && Vector2.Dot(tangentVelocity, Heading) < 0f && tangentVelocity != Vector2.zero)
+        {
+            thrustOutput = true;
+        }
+
+        float turnRate = TurnRate * (2f * Mathf.PI / 360f);
+        float thrustDuration = tangentVelocity.magnitude / Acceleration;
+        float desiredHeadingAngle;
+        if (turnRate * thrustDuration > 1f)
+        {
+            desiredHeadingAngle = 90f;
+        }
+        else
+        {
+            desiredHeadingAngle = (360f / (2f * Mathf.PI)) * Mathf.Acos(1 - turnRate * thrustDuration);
+        }
+        //desiredHeadingAngle = Mathf.Clamp(desiredHeadingAngle, -90f, 90f);
+        if (tangentAngle > 0f)
+        {
+            desiredHeadingAngle *= -1;
+        }
+
+        turnOutput = headingAngle - desiredHeadingAngle;
+        if (turnOutput < 0f)
+        {
+            turnOutput = -1f;
+        }
+        else if (turnOutput > 0f)
+        {
+            turnOutput = 1f;
+        }
+
+        DesiredHeading = desiredHeadingAngle;
+        DesiredHeadingOutputText.GetComponent<Text>().text = "(DesiredHeading = "+ DesiredHeading.ToString()+")";
+        ThrustOutputText.GetComponent<Text>().text = "Thrust: " + thrustOutput.ToString();
+        BreakOutputText.GetComponent<Text>().text = "Break: " + breakOutput.ToString();
+        TurnOutputText.GetComponent<Text>().text = "Turn: " + turnOutput.ToString();
+    }
+}
